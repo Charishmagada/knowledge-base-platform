3360eeea195abb4ec083f89d1b02c5aa3d28c676
Charishmagada <charishmagada@gmail.com>
2025-07-01 22:48:48 +0530

    Define User and Document models using SQLAlchemy

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,4 @@
+# models.py
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -7,12 +8,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
-    documents = db.relationship('Document', backref='author', lazy=True)
+    documents = db.relationship("Document", backref="author", lazy=True)
 
 class Document(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text, nullable=False)
     is_public = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
