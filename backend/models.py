6dbf68c9c719790b7e9b856f7c1c61e59bcd4d25
Charishmagada <charishmagada@gmail.com>
2025-07-03 23:43:09 +0530

    Update Document model to use timezone-aware datetime for created_at and updated_at

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,13 @@
 # models.py
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime,timezone
+import pytz
+
+IST = pytz.timezone("Asia/Kolkata")
+
+def get_ist_time():
+    return datetime.now(IST)
+
 
 db = SQLAlchemy()
 
@@ -9,12 +16,14 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
     documents = db.relationship("Document", backref="author", lazy=True)
+from datetime import datetime
 
 class Document(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text, nullable=False)
     is_public = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
-    author_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+    created_at = db.Column(db.DateTime, default=get_ist_time)
+    updated_at = db.Column(db.DateTime, default=get_ist_time, onupdate=get_ist_time)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
